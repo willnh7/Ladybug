068ffcc904a41137a403b7054c28b3f21e027be1
willnh7 <sarmientowillsa@gmail.com>
2026-01-08 10:36:40 -0800

    Pseudocode and idea for what needs to be done has been created

---
+++ Leaf.py
@@ -0,0 +1,40 @@
+## The Leaf python file will be mainly about somehow integrating 
+# the chatgpt ai api into an essentially console terminal prompting.
+
+
+Pseudocode {
+    Output a prompt for the user to input a question 
+    Read in string from console for the prompt 
+    output response from ai to console,
+    ask again
+
+
+
+
+
+    do {
+        output to console for the user to ask a question
+        read in user input as string  then plug into chatgpt 
+        output the response from ai 
+    } while(user wants another response)
+
+
+
+
+
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
